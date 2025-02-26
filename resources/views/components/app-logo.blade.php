d1d5fe733a7246c1aff65bad2e577424cb878347
Andy Newhouse <hi@andymnewhouse.me>
2025-02-25 18:08:26 -0600

    Add logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,4 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
-</div>
+<x-app-logo-icon class="size-8 fill-current text-accent-content dark:text-accent-foreground" />
 <div class="ml-1 grid flex-1 text-left text-sm">
-    <span class="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+    <span class="mb-0.5 truncate leading-none font-semibold">Card Party</span>
 </div>
