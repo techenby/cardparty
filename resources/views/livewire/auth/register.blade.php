8be8ab82f4d7e38012f265681b76d4a30ae9f56e
Andy Newhouse <hi@andymnewhouse.me>
2025-02-28 18:49:22 -0600

    Redirect intended on register

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -31,7 +31,7 @@ new #[Layout('components.layouts.auth')] class extends Component {
 
         Auth::login($user);
 
-        $this->redirect(route('dashboard', absolute: false), navigate: true);
+        $this->redirectIntended(route('dashboard', absolute: false), navigate: true);
     }
 }; ?>
 
