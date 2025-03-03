b8823799d267af9b90a65733c91b30f5f9009984
Andy Newhouse <hi@andymnewhouse.me>
2025-03-03 17:59:43 -0600

    Add rounded background like homepage

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -5,13 +5,13 @@
 </head>
 <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
 <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
-    <div class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r dark:border-neutral-800">
-        <div class="absolute inset-0 bg-neutral-900"></div>
+    <div class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex">
+        <div class="bg-i-like-cards absolute inset-2 rounded-4xl ring-1 ring-black/5 ring-inset"></div>
         <a href="{{ route('home') }}" class="relative z-20 flex items-center text-lg font-medium" wire:navigate>
-                    <span class="flex h-10 w-10 items-center justify-center rounded-md">
-                        <x-app-logo-icon class="mr-2 h-7 fill-current text-white" />
-                    </span>
-            {{ config('app.name', 'Laravel') }}
+            <span class="flex h-10 w-10 items-center justify-center rounded-md">
+                <x-app-logo-icon class="mr-2 h-7 fill-current text-white" />
+            </span>
+            Card Party
         </a>
     </div>
     <div class="w-full lg:p-8">
