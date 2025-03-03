4d3d2570935e9c86c814ffef349c6cf0948d403f
Andy Newhouse <hi@andymnewhouse.me>
2025-03-03 14:49:33 -0600

    Work on placeholder landing page

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -4,7 +4,7 @@
 <title>{{ $title ?? 'Card Party' }}</title>
 
 <link rel="preconnect" href="https://fonts.bunny.net">
-<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600|vt323:400" rel="stylesheet" />
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
