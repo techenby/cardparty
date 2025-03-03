dc7ae743e6057a6d2d19ac0407c1eed58d1d9c20
Andy Newhouse <hi@andymnewhouse.me>
2025-03-03 17:57:57 -0600

    Revert auth split order

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -5,18 +5,6 @@
 </head>
 <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
 <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
-    <div class="w-full lg:p-8">
-        <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <a href="{{ route('home') }}" class="z-20 flex flex-col items-center gap-2 font-medium lg:hidden" wire:navigate>
-                <span class="flex h-9 w-9 items-center justify-center rounded-md">
-                    <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
-                </span>
-
-                <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
-            </a>
-            {{ $slot }}
-        </div>
-    </div>
     <div class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r dark:border-neutral-800">
         <div class="absolute inset-0 bg-neutral-900"></div>
         <a href="{{ route('home') }}" class="relative z-20 flex items-center text-lg font-medium" wire:navigate>
@@ -35,6 +23,18 @@
                 <p class="text-lg">&ldquo;{{ trim($message) }}&rdquo;</p>
                 <footer class="text-sm">{{ trim($author) }}</footer>
             </blockquote>
+        </div>
+    </div>
+    <div class="w-full lg:p-8">
+        <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <a href="{{ route('home') }}" class="z-20 flex flex-col items-center gap-2 font-medium lg:hidden" wire:navigate>
+                <span class="flex h-9 w-9 items-center justify-center rounded-md">
+                    <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
+                </span>
+
+                <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
+            </a>
+            {{ $slot }}
         </div>
     </div>
 </div>
