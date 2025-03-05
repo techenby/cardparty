633cdf2d77f176ee3d812ff4a15333205f2a5059
Andy Newhouse <hi@andymnewhouse.me>
2025-03-04 18:49:33 -0600

    Stub out event

---
+++ app/Events/GameStarted.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Events;
+
+use Thunk\Verbs\Event;
+
+class GameStarted extends Event
+{
+    public function handle()
+    {
+        // I play the hand that was dealt, I got a deck full of aces. I gave birth to your style, I need a check for my labor - Lil Wayne
+    }
+}
